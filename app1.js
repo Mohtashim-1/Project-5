6efffb74f1fb72605e48b1542024fa4a75fa2c0f
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2020-06-14 15:47:59 +0500

    Project 5 Completed

---
+++ app1.js
@@ -0,0 +1,10 @@
+alert("Error! Please enter a valid password");
+alert("Welcome to JS Land...\nHappy Coding!");
+alert("Welcome to JS Land...");
+alert("Happy Coding!\n","Prevent this page from creating additional dialogs.")
+
+var a = "Hello... I can run JS thought my web browser's console";
+console.log(a);
+
+// Practice placement of <script></script> element in following sections of your project in exercise 6:
+// Answer.Body (before your page’s HTML)
---
+++ app2.js
@@ -0,0 +1,36 @@
+var username="Mohtashim";
+var myname;
+myname="Mohtashim" ;
+
+var message;
+message="Hello World";
+alert(message);
+
+var name="Jhone Doe";
+alert(name);
+
+var age="15 years old";
+alert(age);
+
+var certifite="Certified Mobile App Development";
+alert(certifite);
+
+var pizza="Pizza\nPizz\nPiz\nPi\nP";
+alert(pizza);
+
+
+var text="My email address is ";
+var email="example@example.com";
+alert(text+email);
+
+var book;
+book="I am trying to learn from the Book A smarter way to learn JavaScript";
+alert(book);
+
+
+document.write("Yah! I can write HTML content through JavaScript");
+
+var a="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(a);
+
+
